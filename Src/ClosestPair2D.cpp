a57053fb589674c9dcf70ebde22ff4caedcd5808
flotisable <s09930698@gmail.com>
2018-12-15 20:32:45 +0800

    add algorithm detail

--- Src/ClosestPair2D.cpp
+++ Src/ClosestPair2D.cpp
@@ -2,6 +2,8 @@
 
 #include <sstream>
 #include <algorithm>
+#include <cassert>
+#include <limits>
 using namespace std;
 
 #include "Select.h"
@@ -50,6 +52,29 @@ std::string ClosestPair2D::result()
 // private member functions
 ClosestPair2D::SolveResult ClosestPair2D::solve( const PointIndexes &pointIndexes )
 {
+  assert( !pointIndexes.empty() ); // precondition
+
+  SolveResult resultMerge;
+
+  if( pointIndexes.size() <= 2 )
+  {
+    if( pointIndexes.size() == 1 )
+    {
+      resultMerge.distanceSquare = numeric_limits<Value>::max();
+    }
+    else // 2 points
+    {
+      PointPair pointPair = make_pair( pointIndexes.front(), pointIndexes.back() );
+
+      if( pointPair.first > pointPair.second )
+        swap( pointPair.first, pointPair.second );
+
+      resultMerge.distanceSquare = distanceSquare( mPoints[pointPair.first], mPoints[pointPair.second] );
+      resultMerge.pairs.push_back( pointPair );
+    }
+    return resultMerge;
+  }
+
   PointIndexes  pointIndexesLeft;
   PointIndexes  pointIndexesRight;
   vector<Value> xs;
@@ -58,9 +83,8 @@ ClosestPair2D::SolveResult ClosestPair2D::solve( const PointIndexes &pointIndexe
   Value       xMedian;
   SolveResult resultLeft;
   SolveResult resultRight;
-  SolveResult resultMerge;
+  Value       distance;
 
-  /*
   for( size_t i : pointIndexes )
      xs.push_back( mPoints[i].x );
 
@@ -74,8 +98,56 @@ ClosestPair2D::SolveResult ClosestPair2D::solve( const PointIndexes &pointIndexe
 
   resultLeft  = solve( pointIndexesLeft   );
   resultRight = solve( pointIndexesRight  );
-  */
 
+  if( resultLeft.distanceSquare == resultRight.distanceSquare )
+  {
+    resultMerge = resultLeft;
+    resultMerge.pairs.insert( resultMerge.pairs.end(), resultRight.pairs.begin(), resultRight.pairs.end() );
+  }
+  else
+    resultMerge = ( resultLeft.distanceSquare < resultRight.distanceSquare ) ? resultLeft: resultRight;
+
+  distance = sqrt( resultMerge.distanceSquare );
+
+  for( size_t i = 0, j = 0 ; i < pointIndexesLeft.size() ; ++i )
+  {
+     const Point<Value> &pLeft = mPoints[pointIndexesLeft[i]];
+
+     for( ; j > 0 ; --j )
+     {
+        const Point<Value> &pRight = mPoints[pointIndexesRight[j]];
+
+        if( pRight.y - pLeft.y > distance ) break;
+     }
+
+     for( ; j < pointIndexesRight.size() ; ++j )
+     {
+        const Point<Value> &pRight = mPoints[pointIndexesRight[j]];
+
+        if( pRight.y - pLeft.y > distance ) continue;
+        if( pLeft.y - pRight.y > distance ) break;
+
+        Value distanceS = distanceSquare( pLeft, pRight );
+
+        if( resultMerge.distanceSquare < distanceS  ) continue;
+
+        PointPair pointPair = make_pair( pointIndexesLeft[i], pointIndexesRight[j] );
+
+        if( pointPair.first > pointPair.second )
+          swap( pointPair.first, pointPair.second );
+
+        if( resultMerge.distanceSquare > distanceS )
+        {
+          resultMerge.distanceSquare = distanceS;
+          resultMerge.pairs.resize( 1 );
+          resultMerge.pairs.front() = pointPair;
+        }
+        else // equal
+        {
+          resultMerge.pairs.push_back( pointPair );
+        }
+     }
+  }
   return resultMerge;
 }
 // end private member functions

8d3ee71cbde632238db6cdc5d7bc0680a753e170
flotisable <s09930698@gmail.com>
2018-12-15 17:19:05 +0800

    add io

--- Src/main.cpp
+++ Src/main.cpp
@@ -6,6 +6,23 @@ using namespace std;
 int main()
 {
   ClosestPair2D solver;
+  int           pointNum;
+
+  cin >> pointNum;
+
+  solver.setPointNum( pointNum );
+
+  for( int i = 0 ; i < pointNum ; ++i )
+  {
+     ClosestPair2D::Value x, y;
+
+     cin >> x >> y;
+
+     solver.addPoint( x, y );
+  }
+  solver.solve();
+
+  cout << solver.result() << "\n";
 
   return 0;
 }

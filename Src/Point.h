114c9bfee7f86a22e046b671a7f06c80a0e38ac4
flotisable <s09930698@gmail.com>
2018-12-15 17:06:19 +0800

    change point to template

--- Src/Point.h
+++ Src/Point.h
@@ -3,13 +3,15 @@
 
 #include <cmath>
 
+template<class Value>
 struct Point
 {
-  long x;
-  long y;
-}
+  Value x;
+  Value y;
+};
 
-inline long distance( const Point &a, const Point &b )
+template<class Value>
+inline Value distance( const Point<Value> &a, const Point<Value> &b )
 {
   return std::sqrt( std::pow( a.x - b.x, 2 ) + std::pow( a.y - b.y, 2 ) );
 }
